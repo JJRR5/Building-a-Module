839a820e439fd249cc9f4df1de8cd5a17edcea7e
JJRR5 <jramirez@vauxoo.com>
2022-08-12 15:05:25 -0500

    [IMP] define demonstration data

--- open_academy/models/course.py
+++ open_academy/models/course.py
@@ -4,6 +4,6 @@ from odoo import models,fields
 class course(models.Model):
     _name = 'open_academy.course'
     _description = 'open_academy.course'
-    _title = "Javascript for beginners"
+    # _title = "Javascript for beginners"
     
     
