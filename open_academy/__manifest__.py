ccf16223c19d04459181e0ee3114ada04b6271e1
JJRR5 <jramirez@vauxoo.com>
2022-08-12 08:44:39 -0500

    [IMP] added core files for the module

---
+++ open_academy/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "open_academy",
+
+    'summary': "Module objective is to create test courses",
+
+    'description': "This is a test module for an Open Academy Courses",
+
+    'author': "JJRR Companny",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        'views/views.xml',
+        'views/templates.xml',
+        'security/ir.model.access.csv',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
