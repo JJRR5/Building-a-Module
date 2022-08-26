1ba192721b9bd8039844c45ec67231b198324238
JJRR5 <jramirez@vauxoo.com>
2022-08-26 14:40:44 -0500

    [IMP] created a method that notifies an instructor cannot be an attendee of his own session

--- open_academy/models/course.py
+++ open_academy/models/course.py
@@ -10,3 +10,19 @@ class Course(models.Model):
     description = fields.Text()
     responsible_user_id = fields.Many2one('res.users')
     session_ids = fields.One2many('session', 'course_id')
+    _sql_constraints = [
+        ('unique_course_name', 'UNIQUE (name)', 'This course already exists'),
+        (
+            'check_title_description',
+            'CHECK (title NOT LIKE description)',
+            'The title must be different than the description'
+        )
+    ]
+
+    def copy(self, default=None):
+        default = default or {}
+        copied_count = self.search_count([('name', '=like', 'Copy of %s' % self.name)])
+        new_name = 'Copy of %s' % self.name
+        new_name += '(%s)' % copied_count if copied_count > 0 else ''
+        default.update({'name': new_name})
+        return super().copy(default=default)
